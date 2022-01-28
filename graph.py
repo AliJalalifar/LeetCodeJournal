3114eb95b5d29cbbc575248cbba7efea72867f76
Ali Jalalifar <1jalalifar@gmail.com>
2022-01-28 16:03:39 -0500

    has_path added! connected_components_count added! all functions inside Graph class now accept both directional and un-directional graph! cyclic graph support added! to-do: work on connected_components_count

--- graph.py
+++ graph.py
@@ -1,44 +1,99 @@
-class DirectionalGraph:
+class Graph:
 
     def __init__(self, edges):
         self.edges = edges
-        self.dict_graph = {}
-        for start, end in self.edges:
-            if start in self.dict_graph:
-                self.dict_graph[start].append(end)
-            else:
-                self.dict_graph[start] = [end]
+        self.dir_graph_dict = self.do_dir_graph_dict()
+        self.undir_graph_dict = self.do_undir_graph_dict()
 
-    def get_path(self, start, end, path=[]):
+    def do_dir_graph_dict(self):
+        dict_graph = {}
+        for start, end in self.edges:
+            if start in dict_graph:
+                dict_graph[start].append(end)
+            else:
+                dict_graph[start] = [end]
+        return dict_graph
+
+    def do_undir_graph_dict(self):
+        dict_graph = {}
+        for start, end in self.edges:
+            if start in dict_graph:
+                dict_graph[start].append(end)
+            else:
+                dict_graph[start] = [end]
+            if end in dict_graph:
+                dict_graph[end].append(start)
+            else:
+                dict_graph[end] = [start]
+        return dict_graph
+
+    def get_path(self, graph, start, end, path=[]):
         path = path + [start]
         if start == end:
             return [path]
-        if start not in self.dict_graph:
+        if start not in graph:
             return []
         paths = []
-        for node in self.dict_graph[start]:
+        for node in graph[start]:
             if node not in path:
-                new_paths = self.get_path(node, end, path)
+                new_paths = self.get_path(graph, node, end, path)
                 for p in new_paths:
                     paths.append(p)
 
         return paths
 
-    def dfs_recursion(self, source):
+    def dfs_recur(self, graph, source):
         print(source)
-        if source not in self.dict_graph:
-            return
-        for neighbour in self.dict_graph[source]:
-            self.dfs_recursion(neighbour)
+        if source in graph:
+            for neighbour in graph[source]:
+                self.dfs_recur(graph, neighbour)
 
-    def dfs_iteration(self, source):
+    def dfs_itr(self, graph, source):
         stack = [source]
         while stack:
             current = stack.pop()
             print(current)
-            if current in self.dict_graph:
-                for neighbor in self.dict_graph[current]:
+            if current in graph:
+                for neighbor in graph[current]:
                     stack.append(neighbor)
+
+    def bfs_itr(self, graph, source):
+        queue = [source]
+        while queue:
+            current = queue.pop()
+            print(current)
+            if current in graph:
+                for node in graph[current]:
+                    queue.insert(0, node)
+
+    def has_path(self, graph, src, dst, visited_nodes=set()):
+
+        if src in visited_nodes:
+            return False
+
+        visited_nodes.add(src)
+
+        if src == dst:
+            return True
+
+        if src in graph:
+            for neighbor in graph[src]:
+                if self.has_path(graph, neighbor, dst, visited_nodes):
+                    return True
+
+        return False
+
+    def connected_components_count(self, graph):
+        connected = set()
+        count = 0
+        for src in graph:
+            if src not in connected:
+                connected.add(src)
+                for dst in graph:
+                    if self.has_path(graph,src, dst, visited_nodes=set()):
+                        connected.add(dst)
+                count += 1
+        return count
 
 
 if __name__ == "__main__":
@@ -52,12 +107,13 @@ if __name__ == "__main__":
     #     ("Istanbul", "Tehran")
     # ]
     routes = [
-        ("a", "b"),
-        ("a", "c"),
-        ("b", "d"),
-        ("d", "f"),
-        ("c", "e")
+        ("1", "2"),
+        ("4", "6"),
+        ("5", "6"),
+        ("6", "8"),
+        ("7", "6"),
+        ["3", "3"]
     ]
-    routeGraph = DirectionalGraph(routes)
-    # print(routeGraph.dfs("Toronto"))
-    routeGraph.dfs_iteration("a")
+    routeGraph = Graph(routes)
+    # # print(routeGraph.dfs("Toronto"))
+    print(routeGraph.connected_components_count(routeGraph.undir_graph_dict))
